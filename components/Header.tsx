2e7f2121349a2dea9ff0dafa47660651a2d4314a
Arpit Namdev <arpit8392@gmail.com>
2023-06-15 22:51:36 +0530

    Mobile Menu Image Fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -190,24 +190,17 @@ export default function Header() {
 				className='md:hidden'
 				open={mobileMenuOpen}
 				onClose={setMobileMenuOpen}>
-				<div className='fixed inset-0 z-10' />
-				<Dialog.Panel className='fixed inset-y-0 right-0 z-10 flex w-full flex-col justify-between overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+				<div className='fixed inset-0 z-10 bg-offBlack opacity-75 ' />
+				<Dialog.Panel className='fixed inset-y-0 right-0 z-10 flex w-2/3 flex-col justify-between overflow-y-auto bg-white '>
 					<div className='p-6'>
 						<div className='flex items-center justify-between'>
-							<a href='#' className='-m-1.5 p-1.5'>
-								<span className='sr-only'>Your Company</span>
+							<button type='button' onClick={() => setMobileMenuOpen(false)}>
+								<span className='sr-only'>Close menu</span>
 								<Image
-									src={Logo}
-									alt='snap logo'
+									src={CloseIcon}
+									alt=''
 									className='w-auto object-contain'
 								/>
-							</a>
-							<button
-								type='button'
-								className='-m-2.5 rounded-md p-2.5 text-gray-700'
-								onClick={() => setMobileMenuOpen(false)}>
-								<span className='sr-only'>Close menu</span>
-								<XMarkIcon className='h-6 w-6' aria-hidden='true' />
 							</button>
 						</div>
 						<div className='mt-6 flow-root'>
