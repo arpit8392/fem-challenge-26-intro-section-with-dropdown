ec06d06caa69e94e1eb1bb3a47fdb4e7b298742c
Arpit Namdev <arpit8392@gmail.com>
2023-06-15 22:41:59 +0530

    Complete Desktop Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,81 +2,57 @@
 
 import { Fragment, useState } from 'react'
 import { Dialog, Popover, Transition } from '@headlessui/react'
-import {
-	ArrowPathIcon,
-	Bars3Icon,
-	ChartPieIcon,
-	CursorArrowRaysIcon,
-	FingerPrintIcon,
-	SquaresPlusIcon,
-	XMarkIcon,
-} from '@heroicons/react/24/outline'
-import {
-	ChevronDownIcon,
-	PhoneIcon,
-	PlayCircleIcon,
-} from '@heroicons/react/20/solid'
 
-const products = [
+import MenuIcon from '@/public/images/icon-menu.svg'
+import CloseIcon from '@/public/images/icon-close-menu.svg'
+
+import TodoIcon from '@/public/images/icon-todo.svg'
+import CalendarIcon from '@/public/images/icon-calendar.svg'
+import ReminderIcon from '@/public/images/icon-reminders.svg'
+import PlanningIcon from '@/public/images/icon-planning.svg'
+
+import ArrowUpIcon from '@/public/images/icon-arrow-up.svg'
+import ArrowDownIcon from '@/public/images/icon-arrow-down.svg'
+
+import Logo from '@/public/images/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const features = [
 	{
-		name: 'Analytics',
-		description: 'Get a better understanding of your traffic',
+		name: 'Todo List',
 		href: '#',
-		icon: ChartPieIcon,
+		icon: TodoIcon,
 	},
 	{
-		name: 'Engagement',
-		description: 'Speak directly to your customers',
+		name: 'Calendar',
 		href: '#',
-		icon: CursorArrowRaysIcon,
+		icon: CalendarIcon,
 	},
 	{
-		name: 'Security',
-		description: 'Your customers’ data will be safe and secure',
+		name: 'Reminders',
 		href: '#',
-		icon: FingerPrintIcon,
+		icon: ReminderIcon,
 	},
 	{
-		name: 'Integrations',
-		description: 'Connect with third-party tools',
+		name: 'Planning',
 		href: '#',
-		icon: SquaresPlusIcon,
-	},
-	{
-		name: 'Automations',
-		description: 'Build strategic funnels that will convert',
-		href: '#',
-		icon: ArrowPathIcon,
+		icon: PlanningIcon,
 	},
 ]
-const callsToAction = [
-	{ name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-	{ name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
+
 const company = [
 	{
-		name: 'About us',
+		name: 'History',
 		href: '#',
-		description:
-			'Learn more about our company values and mission to empower others',
 	},
 	{
-		name: 'Careers',
+		name: 'Our Team',
 		href: '#',
-		description:
-			'Looking for you next career opportunity? See all of our open positions',
-	},
-	{
-		name: 'Support',
-		href: '#',
-		description:
-			'Get in touch with our dedicated support team or reach out on our community forums',
 	},
 	{
 		name: 'Blog',
 		href: '#',
-		description:
-			'Read our latest announcements and get perspectives from our team',
 	},
 ]
 
@@ -85,141 +61,133 @@ export default function Header() {
 
 	return (
 		<header className='p-4 md:px-10 md:py-6'>
-			<nav
-				className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
-				aria-label='Global'>
-				<div className='flex lg:flex-1'>
-					<a href='#' className='-m-1.5 p-1.5'>
-						<span className='sr-only'>Your Company</span>
-						<img
-							className='h-8 w-auto'
-							src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-							alt=''
-						/>
-					</a>
+			<nav className='flex items-center justify-between' aria-label='Global'>
+				<div className='flex items-center md:gap-16'>
+					<div>
+						<span className='sr-only'>Snap</span>
+						<Image src={Logo} alt='snap' className='w-auto object-contain' />
+					</div>
+
+					<Popover.Group className='hidden md:flex md:gap-x-10'>
+						<Popover className='relative'>
+							{({ open }) => (
+								<>
+									<Popover.Button className='flex items-center gap-x-2 text-sm font-medium text-mediumGray hover:text-offBlack'>
+										Features
+										<Image
+											src={open ? ArrowUpIcon : ArrowDownIcon}
+											alt=''
+											className='w-auto object-contain'
+										/>
+									</Popover.Button>
+
+									<Transition
+										as={Fragment}
+										enter='transition ease-out duration-200'
+										enterFrom='opacity-0 translate-y-1'
+										enterTo='opacity-100 translate-y-0'
+										leave='transition ease-in duration-150'
+										leaveFrom='opacity-100 translate-y-0'
+										leaveTo='opacity-0 translate-y-1'>
+										<Popover.Panel className='absolute -left-20 top-full z-10 mt-5 w-40 rounded-xl bg-white p-6 shadow-lg'>
+											{features.map((item) => (
+												<div
+													key={item.name}
+													className='relative flex gap-4 rounded-lg py-2'>
+													<Image
+														src={item.icon}
+														alt=''
+														className='w-auto object-contain'
+													/>
+													<Link
+														href={item.href}
+														className='text-sm text-mediumGray hover:text-offBlack'>
+														{item.name}
+														<span className='absolute inset-0' />
+													</Link>
+												</div>
+											))}
+										</Popover.Panel>
+									</Transition>
+								</>
+							)}
+						</Popover>
+
+						<Popover className='relative'>
+							{({ open }) => (
+								<>
+									<Popover.Button className='flex items-center gap-x-2 text-sm font-medium text-mediumGray hover:text-offBlack'>
+										Company
+										<Image
+											src={open ? ArrowUpIcon : ArrowDownIcon}
+											alt=''
+											className='w-auto object-contain'
+										/>
+									</Popover.Button>
+
+									<Transition
+										as={Fragment}
+										enter='transition ease-out duration-200'
+										enterFrom='opacity-0 translate-y-1'
+										enterTo='opacity-100 translate-y-0'
+										leave='transition ease-in duration-150'
+										leaveFrom='opacity-100 translate-y-0'
+										leaveTo='opacity-0 translate-y-1'>
+										<Popover.Panel className='absolute -left-2 top-full z-10 mt-5 w-28 rounded-xl bg-white p-6 shadow-lg'>
+											{company.map((item) => (
+												<div
+													key={item.name}
+													className='relative rounded-lg py-2'>
+													<Link
+														href={item.href}
+														className='text-sm text-mediumGray hover:text-offBlack'>
+														{item.name}
+														<span className='absolute inset-0' />
+													</Link>
+												</div>
+											))}
+										</Popover.Panel>
+									</Transition>
+								</>
+							)}
+						</Popover>
+
+						<a
+							href='#'
+							className='text-sm font-medium text-mediumGray hover:text-offBlack'>
+							Careers
+						</a>
+						<a
+							href='#'
+							className='text-sm font-medium text-mediumGray hover:text-offBlack'>
+							About
+						</a>
+					</Popover.Group>
 				</div>
-				<div className='flex lg:hidden'>
-					<button
-						type='button'
-						className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
-						onClick={() => setMobileMenuOpen(true)}>
+
+				<div className='md:hidden'>
+					<button type='button' onClick={() => setMobileMenuOpen(true)}>
 						<span className='sr-only'>Open main menu</span>
-						<Bars3Icon className='h-6 w-6' aria-hidden='true' />
+						<Image src={MenuIcon} alt='' className='w-auto object-contain' />
 					</button>
 				</div>
-				<Popover.Group className='hidden lg:flex lg:gap-x-12'>
-					<Popover className='relative'>
-						<Popover.Button className='flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900'>
-							Product
-							<ChevronDownIcon
-								className='h-5 w-5 flex-none text-gray-400'
-								aria-hidden='true'
-							/>
-						</Popover.Button>
 
-						<Transition
-							as={Fragment}
-							enter='transition ease-out duration-200'
-							enterFrom='opacity-0 translate-y-1'
-							enterTo='opacity-100 translate-y-0'
-							leave='transition ease-in duration-150'
-							leaveFrom='opacity-100 translate-y-0'
-							leaveTo='opacity-0 translate-y-1'>
-							<Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5'>
-								<div className='p-4'>
-									{products.map((item) => (
-										<div
-											key={item.name}
-											className='group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'>
-											<div className='mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
-												<item.icon
-													className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
-													aria-hidden='true'
-												/>
-											</div>
-											<div className='flex-auto'>
-												<a
-													href={item.href}
-													className='block font-semibold text-gray-900'>
-													{item.name}
-													<span className='absolute inset-0' />
-												</a>
-												<p className='mt-1 text-gray-600'>{item.description}</p>
-											</div>
-										</div>
-									))}
-								</div>
-								<div className='grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50'>
-									{callsToAction.map((item) => (
-										<a
-											key={item.name}
-											href={item.href}
-											className='flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100'>
-											<item.icon
-												className='h-5 w-5 flex-none text-gray-400'
-												aria-hidden='true'
-											/>
-											{item.name}
-										</a>
-									))}
-								</div>
-							</Popover.Panel>
-						</Transition>
-					</Popover>
-
-					<a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-						Features
-					</a>
-					<a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-						Marketplace
-					</a>
-
-					<Popover className='relative'>
-						<Popover.Button className='flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900'>
-							Company
-							<ChevronDownIcon
-								className='h-5 w-5 flex-none text-gray-400'
-								aria-hidden='true'
-							/>
-						</Popover.Button>
-
-						<Transition
-							as={Fragment}
-							enter='transition ease-out duration-200'
-							enterFrom='opacity-0 translate-y-1'
-							enterTo='opacity-100 translate-y-0'
-							leave='transition ease-in duration-150'
-							leaveFrom='opacity-100 translate-y-0'
-							leaveTo='opacity-0 translate-y-1'>
-							<Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-96 rounded-3xl bg-white p-4 shadow-lg ring-1 ring-gray-900/5'>
-								{company.map((item) => (
-									<div
-										key={item.name}
-										className='relative rounded-lg p-4 hover:bg-gray-50'>
-										<a
-											href={item.href}
-											className='block text-sm font-semibold leading-6 text-gray-900'>
-											{item.name}
-											<span className='absolute inset-0' />
-										</a>
-										<p className='mt-1 text-sm leading-6 text-gray-600'>
-											{item.description}
-										</p>
-									</div>
-								))}
-							</Popover.Panel>
-						</Transition>
-					</Popover>
-				</Popover.Group>
-				<div className='hidden lg:flex lg:flex-1 lg:justify-end'>
-					<a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
-						Log in <span aria-hidden='true'>&rarr;</span>
-					</a>
+				<div className='hidden md:flex md:items-center md:gap-x-10'>
+					<Link
+						href={'#'}
+						className='text-sm text-mediumGray hover:text-offBlack'>
+						Login
+					</Link>
+					<Link
+						href={'#'}
+						className='rounded-[14px] border-[1.5px] border-mediumGray px-6 py-3 text-sm text-mediumGray hover:border-offBlack hover:text-offBlack'>
+						Register
+					</Link>
 				</div>
 			</nav>
 			<Dialog
 				as='div'
-				className='lg:hidden'
+				className='md:hidden'
 				open={mobileMenuOpen}
 				onClose={setMobileMenuOpen}>
 				<div className='fixed inset-0 z-10' />
@@ -228,10 +196,10 @@ export default function Header() {
 						<div className='flex items-center justify-between'>
 							<a href='#' className='-m-1.5 p-1.5'>
 								<span className='sr-only'>Your Company</span>
-								<img
-									className='h-8 w-auto'
-									src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-									alt=''
+								<Image
+									src={Logo}
+									alt='snap logo'
+									className='w-auto object-contain'
 								/>
 							</a>
 							<button
@@ -245,15 +213,16 @@ export default function Header() {
 						<div className='mt-6 flow-root'>
 							<div className='-my-6 divide-y divide-gray-500/10'>
 								<div className='space-y-2 py-6'>
-									{products.map((item) => (
+									{features.map((item) => (
 										<a
 											key={item.name}
 											href={item.href}
 											className='group -mx-3 flex items-center gap-x-6 rounded-lg p-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
 											<div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
-												<item.icon
-													className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
-													aria-hidden='true'
+												<Image
+													src={item.icon}
+													alt=''
+													className='w-auto object-contain'
 												/>
 											</div>
 											{item.name}
@@ -290,16 +259,6 @@ export default function Header() {
 								</div>
 							</div>
 						</div>
-					</div>
-					<div className='sticky bottom-0 grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50 text-center'>
-						{callsToAction.map((item) => (
-							<a
-								key={item.name}
-								href={item.href}
-								className='p-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-100'>
-								{item.name}
-							</a>
-						))}
 					</div>
 				</Dialog.Panel>
 			</Dialog>
